acf798b314bd82c6bc249831835a7df0f4f82925
WillyLu326 <luzh0326@gmail.com>
2017-02-21 22:34:40 -0500

    Create experience_item.xml layout

--- app/src/main/java/willy/individual/com/minilinkedinapp/EducationEditActivity.java
+++ app/src/main/java/willy/individual/com/minilinkedinapp/EducationEditActivity.java
@@ -2,11 +2,6 @@ package willy.individual.com.minilinkedinapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 
 import willy.individual.com.minilinkedinapp.models.Education;
