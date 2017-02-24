c52ecfebb7ef4e5f38026a3b1ac745741acd29fe
WillyLu326 <luzh0326@gmail.com>
2017-02-23 20:19:20 -0500

    Pick image from phone

--- app/src/main/java/willy/individual/com/minilinkedinapp/BasicInfoActivity.java
+++ app/src/main/java/willy/individual/com/minilinkedinapp/BasicInfoActivity.java
@@ -1,11 +1,19 @@
 package willy.individual.com.minilinkedinapp;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import willy.individual.com.minilinkedinapp.models.BasicInfo;
+import willy.individual.com.minilinkedinapp.utils.ImageUtils;
+import willy.individual.com.minilinkedinapp.utils.PermissionUtils;
 
 /**
  * Created by zhenglu on 2/22/17.
@@ -14,6 +22,31 @@ import willy.individual.com.minilinkedinapp.models.BasicInfo;
 public class BasicInfoActivity extends BasicEditActivity<BasicInfo>{
 
     public static final String KEY_BASICINFO = "basicinfo";
+    private static final int REQ_CODE_PICK_IMAGE = 100;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_CODE_PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+            Uri imageUri = data.getData();
+            if (imageUri != null) {
+                showImage(imageUri);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PermissionUtils.REQ_CODE_WRITE_EXTERNAL_STORAGE
+                && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            pickImage();
+        }
+    }
 
     @Override
     protected int getLayoutId() {
@@ -34,14 +67,29 @@ public class BasicInfoActivity extends BasicEditActivity<BasicInfo>{
         findViewById(R.id.basicinfo_edit_profile).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (!PermissionUtils.checkPermission(BasicInfoActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    PermissionUtils.requestReadExternalStoragePermission(BasicInfoActivity.this);
+                } else {
+                    pickImage();
+                }
             }
         });
     }
 
     @Override
     protected void setupDefaultUI() {
-
+        findViewById(R.id.basicinfo_edit_profile).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!PermissionUtils.checkPermission(BasicInfoActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    PermissionUtils.requestReadExternalStoragePermission(BasicInfoActivity.this);
+                } else {
+                    pickImage();
+                }
+            }
+        });
     }
 
     @Override
@@ -59,5 +107,20 @@ public class BasicInfoActivity extends BasicEditActivity<BasicInfo>{
         setResult(Activity.RESULT_OK, resultIntent);
 
         finish();
+    }
+
+    private void showImage(@NonNull Uri imageUri) {
+        ImageView imageView = (ImageView) findViewById(R.id.basicinfo_edit_profile);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        imageView.setTag(imageUri);
+        ImageUtils.loadImage(this, imageUri, imageView);
+    }
+
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(Intent.createChooser(intent, "Select picture"),
+                REQ_CODE_PICK_IMAGE);
+
     }
 }
